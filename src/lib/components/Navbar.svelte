70623cb55649673995b15b72d6edc9e5c9e33955
fabri2605 <fabridp77@gmail.com>
2025-10-18 14:19:57 -0300

    favicon

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,36 +1,50 @@
 <script lang="ts">
-  import { 
-    Navbar, 
-    NavBrand,
-    NavLi, 
-    NavUl, 
-    NavHamburger, 
-    Button, 
-    Dropdown, 
-    DropdownItem
- } from "flowbite-svelte";
-  import { ChevronDownOutline } from "flowbite-svelte-icons";
+import {
+  Navbar,
+  NavBrand,
+  NavLi,
+  NavUl,
+  NavHamburger,
+  Button,
+  Dropdown,
+  DropdownItem,
+} from "flowbite-svelte";
+import { ChevronDownOutline } from "flowbite-svelte-icons";
 </script>
 
 <Navbar class="border-b border-neutral-200">
-<!-- BRAND -->
-  <NavBrand  href="/">
-    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">NOMADE</span>
+  <!-- BRAND -->
+  <NavBrand href="/">
+    <img
+      src="/nomade/imagenes/nomade-favicon-dark.svg"
+      alt="Nomade Favicon"
+      class="h-10 w-10"
+    />
+    <span
+      class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+      >NOMADE</span
+    >
   </NavBrand>
 
-<!-- Botones -->
+  <!-- Botones -->
   <div class="flex md:order-2">
-    <Button size="sm" class="bg-emerald-500 rounded-full mr-2 hover:bg-emerald-600 cursor-pointer focus:outline-none focus:ring-0 focus:shadow-none active:shadow-none" > Contacto </Button>
-    <Button size="sm" class="rounded-full cursor-pointer">Emergencias</Button>
+    <Button
+      size="sm"
+      class="mr-2 cursor-pointer rounded-full bg-emerald-500 hover:bg-emerald-600 focus:shadow-none focus:ring-0 focus:outline-none active:shadow-none"
+    >
+      Contacto
+    </Button>
+    <Button size="sm" class="cursor-pointer rounded-full">Emergencias</Button>
     <NavHamburger />
   </div>
 
   <!-- LINKS -->
   <NavUl class="order-1">
-
     <!-- Servicios dropdown -->
     <NavLi class="cursor-pointer text-base  hover:!text-emerald-600">
-      Servicios <ChevronDownOutline class="text-secondary-800 inline h-6 w-6 dark:text-white" />
+      Servicios <ChevronDownOutline
+        class="inline h-6 w-6 text-secondary-800 dark:text-white"
+      />
     </NavLi>
     <Dropdown simple class="w-44">
       <DropdownItem href="/">Consulta a domicilio</DropdownItem>
@@ -41,13 +55,21 @@
       <DropdownItem href="/">Certificados de exportacion</DropdownItem>
     </Dropdown>
 
-    <NavLi class="text-base hover:!text-emerald-600" href="/about">Nosotros</NavLi>
+    <NavLi class="text-base hover:!text-emerald-600" href="/about"
+      >Nosotros</NavLi
+    >
 
-    <NavLi class="text-base hover:!text-emerald-600" href="/docs/components/navbar">Tienda</NavLi>
+    <NavLi
+      class="text-base hover:!text-emerald-600"
+      href="/docs/components/navbar">Tienda</NavLi
+    >
 
-    <NavLi class="text-base hover:!text-emerald-600" href="/pricing">Turnos</NavLi>
+    <NavLi class="text-base hover:!text-emerald-600" href="/pricing"
+      >Turnos</NavLi
+    >
 
-    <NavLi class="text-base hover:!text-emerald-600" href="/contact">Preguntas frecuentes</NavLi>
-
+    <NavLi class="text-base hover:!text-emerald-600" href="/contact"
+      >Preguntas frecuentes</NavLi
+    >
   </NavUl>
 </Navbar>
